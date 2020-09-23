bcb1266ef43d837cfd0c1aeb6223ae553e46c45a
Sorin Mihai Toma <toma.mihai.sorin23@gmail.com>
2020-09-23 20:19:24 +0300

    added the posiblity to skip a question

--- static/app.js
+++ static/app.js
@@ -8,6 +8,8 @@ let gameInfo = document.getElementById('game-info');
 let viewQuestion = document.getElementById('view-question-number');
 let questionNumber = document.getElementById('question-number');
 let answer = document.getElementById('answer');
+let wrongAnswerCount = document.getElementById('wrong-answers-count');
+let score = document.getElementById('score');
 let count = 0;
 let playerPoints = 0;
 let playerLives = 3;
@@ -18,9 +20,9 @@ async function getAsyncQuestion() {
   count++;
   const res = await fetch('http://jservice.io/api/random?count=1');
   const data = await res.json();
-  console.log(data[0].answer);
   questionNumber.innerText = count;
   question.innerText = data[0].question;
+  localStorage.setItem('questionNumber', count);
   localStorage.setItem('currentQuestion', data[0].question);
   localStorage.setItem('currentQuestionAnswer', data[0].answer);
 }
@@ -44,6 +46,8 @@ function checkAnswer() {
   if (answer.value == localStorage.getItem('currentQuestionAnswer')) {
     console.log('correct answer');
     playerPoints += 10;
+    localStorage.setItem('playerPoints', playerPoints);
+    score.innerText = playerPoints;
     console.log(playerPoints);
     let answerAlert = document.createElement('div');
     answerAlert.innerHTML = 'CORRECT';
@@ -58,6 +62,8 @@ function checkAnswer() {
   } else {
     console.log('wrong answer');
     playerLives -= 1;
+    localStorage.setItem('playerLives', playerLives);
+    wrongAnswerCount.innerText = playerLives;
     if (playerLives != 0) {
       let answerAlert = document.createElement('div');
       answerAlert.innerHTML = 'WRONG';
@@ -93,6 +99,29 @@ function checkAnswer() {
   }
 }
 
+// Skip a question
+
+async function skipQuestion() {
+  if (playerPoints != 0) {
+    count++;
+    const res = await fetch('http://jservice.io/api/random?count=1');
+    const data = await res.json();
+    questionNumber.innerText = count;
+    question.innerText = data[0].question;
+    playerPoints -= 5;
+    score.innerText = playerPoints;
+    localStorage.setItem('playerPoints', playerPoints);
+    localStorage.setItem('currentQuestion', data[0].question);
+    localStorage.setItem('currentQuestionAnswer', data[0].answer);
+  } else {
+    skipBtn.innerText = 'You need at least 5 points to skip';
+    setTimeout(function () {
+      skipBtn.innerText = 'Skip :(';
+    }, 2000);
+  }
+}
+
 // Event listeners
 startBtn.addEventListener('click', startGame);
 nextBtn.addEventListener('click', checkAnswer);
+skipBtn.addEventListener('click', skipQuestion);
