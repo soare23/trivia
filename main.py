f7e87c2f1c58c0c0f158f87cfa8bc83b0d99c0d4
Sorin Mihai Toma <toma.mihai.sorin23@gmail.com>
2020-09-23 20:18:59 +0300

    added the route for the leaderboard page

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, request, redirect, render_template, url_for, session, escape, flash
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template('leaderboard')
 
 
 if __name__ == '__main__':
