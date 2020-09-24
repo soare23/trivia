47932216f776682da612c382f956585d526ea01d
Sorin Mihai Toma <toma.mihai.sorin23@gmail.com>
2020-09-24 14:30:46 +0300

    added all 3 pages + psoiblity to save/read into the DB

--- main.py
+++ main.py
@@ -3,14 +3,48 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
-@app.route('/')
+ENV = 'dev'
+
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://soare:2302@localhost/test_db'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Score(db.Model):
+    __tablename__ = 'score'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200))
+    score = db.Column(db.Integer)
+
+    def __init__(self, username, score):
+        self.username = username
+        self.score = score
+
+
+@app.route('/', methods=["GET", "POST"])
 def hello_world():
+    if request.method == "POST":
+        result = request.get_json()
+        username = result['username']
+        user_score = result['score']
+        data = Score(username, user_score)
+        db.session.add(data)
+        db.session.commit()
+        return render_template('index.html')
     return render_template('index.html')
 
 
 @app.route('/leaderboard')
 def leaderboard():
-    return render_template('leaderboard.html')
+    top10 = Score.query.order_by(Score.score.desc()).limit(10).all()
+    return render_template('leaderboard.html', top10=top10)
 
 
 @app.route('/rules')
@@ -19,4 +53,4 @@ def rules():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
